d0a37828bcf81aff97fe4ed9d9dab800f22cf92e
jeongho <pjhpjh0805@naver.com>
2023-12-16 17:28:56 +0900

    Initial commit

---
+++ src/main/java/hoya/buildtask/BuildTaskApplication.java
@@ -0,0 +1,13 @@
+package hoya.buildtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hoya/buildtask/BuildTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package hoya.buildtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
